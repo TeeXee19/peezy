c29083597029ed7ebc8d3a4afab089a1dad22a47
Tochukwu  Franklin Ene <teec3000@gmail.com>
2020-08-21 13:05:47 +0100

    Created the Setup pin page, and adjusted the change-email.html link

--- js/custom.js
+++ js/custom.js
@@ -218,3 +218,57 @@ $(document).ready(function(){
     });
   }); 
 });
+
+
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+  var actions = $(".table3 td:last-child").html();
+  // Append table with add row form on add new button click
+    $(".add-new").click(function(){
+    $(this).attr("disabled", "disabled");
+    var index = $(".table3 tbody tr:last-child").index();
+        var row = '<tr>' +
+            '<td><input type="text" class="form-control" name="Name" id="name"></td>' +
+            '<td><input type="text" class="form-control" name="Number" id="number"></td>' +
+            '<td><input type="text" class="form-control" name="Status" id="status"></td>' +
+      '<td>' + actions + '</td>' +
+        '</tr>';
+      $(".table3").append(row);   
+    $(".table3 tbody tr").eq(index + 1).find(".add, .edit").toggle();
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+  // Add row on add button click
+  $(document).on("click", ".add", function(){
+    var empty = false;
+    var input = $(this).parents("tr").find('input[type="text"]');
+        input.each(function(){
+      if(!$(this).val()){
+        $(this).addClass("error");
+        empty = true;
+      } else{
+                $(this).removeClass("error");
+            }
+    });
+    $(this).parents("tr").find(".error").first().focus();
+    if(!empty){
+      input.each(function(){
+        $(this).parent("td").html($(this).val());
+      });     
+      $(this).parents("tr").find(".add, .edit").toggle();
+      $(".add-new").removeAttr("disabled");
+    }   
+    });
+  // Edit row on edit button click
+  $(document).on("click", ".edit", function(){    
+        $(this).parents("tr").find("td:not(:last-child)").each(function(){
+      $(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
+    });   
+    $(this).parents("tr").find(".add, .edit").toggle();
+    $(".add-new").attr("disabled", "disabled");
+    });
+  // Delete row on delete button click
+  $(document).on("click", ".delete", function(){
+        $(this).parents("tr").remove();
+    $(".add-new").removeAttr("disabled");
+    });
+});
